300d6b0b29a77462b308da87105664bc38268879
Xi Xing <xxing@princeton.edu>
2023-10-25 01:16:48 -0400

    Update msi_get_isoidata.m

--- msi_get_isoidata.m
+++ msi_get_isoidata.m
@@ -15,8 +15,10 @@ function msi=msi_get_isoidata(msi,pk)
       isodt_.idata_fra_nocor=isodt_.idata_norm*(0:n-1)'/(n-1);%fractional labeling, no correction
   elseif pk.isoType==4
       maxM=pk.maxM_;
-      [~,~,tp]=isocorr_CN(isodt_.idata',maxM(1),maxM(2));
+      isodt_.idata_norm=isodt_.idata./sum(isodt_.idata,2);
+      [~,~,tp]=isocorr_CN(isodt_.idata',maxM(1),maxM(2));      
       isodt_.idata_cor=tp';
+      isodt_.idata_cor_original=isodt_.idata_cor.*sum(isodt_.idata,2);
       vec1=repmat(0:maxM(1),1,maxM(2)+1);
       vec2=reshape(repmat(0:maxM(2),maxM(1)+1,1),1,(maxM(1)+1)*(maxM(2)+1));
       isodt_.idata_fra(:,1)= isodt_.idata_cor*vec1'/maxM(1);
